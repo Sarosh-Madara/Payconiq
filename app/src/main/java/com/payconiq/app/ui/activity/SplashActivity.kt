b4771c42df9e057acbb81f5aa2ccbf77d52950fa
saroshvt <sarosh.madara@vektortelekom.com>
2022-04-27 23:42:31 +0500

    MSM update, Security for Root and jail broken, minification and obfuscation

--- app/src/main/java/com/payconiq/app/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/payconiq/app/ui/activity/SplashActivity.kt
@@ -1,11 +1,15 @@
 package com.payconiq.app.ui.activity
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.payconiq.app.R
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
 import java.util.*
 
 
@@ -15,6 +19,12 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         this@SplashActivity.supportActionBar?.hide()
         setContentView(R.layout.activity_splash)
+
+        if(checkRootMethod1() && checkRootMethod2() && checkRootMethod3()){
+
+        }else{
+            finish()
+        }
 
         Timer().schedule(object : TimerTask() {
             override fun run() {
@@ -35,5 +45,35 @@ class SplashActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
+    }
+
+    private fun checkRootMethod1(): Boolean {
+        val buildTags = Build.TAGS
+        return buildTags != null && buildTags.contains("test-keys")
+    }
+
+    private fun checkRootMethod2(): Boolean {
+        val paths = arrayOf(
+            "/system/app/Superuser.apk", "/sbin/su", "/system/bin/su", "/system/xbin/su",
+            "/data/local/xbin/su", "/data/local/bin/su", "/system/sd/xbin/su",
+            "/system/bin/failsafe/su", "/data/local/su", "/su/bin/su"
+        )
+        for (path in paths) {
+            if (File(path).exists()) return true
+        }
+        return false
+    }
+
+    private fun checkRootMethod3(): Boolean {
+        var process: Process? = null
+        return try {
+            process = Runtime.getRuntime().exec(arrayOf("/system/xbin/which", "su"))
+            val `in` = BufferedReader(InputStreamReader(process.inputStream))
+            `in`.readLine() != null
+        } catch (t: Throwable) {
+            false
+        } finally {
+            process?.destroy()
+        }
     }
 }
